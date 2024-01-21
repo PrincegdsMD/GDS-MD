5ae2f0c391dd7a5699dd2dfe7fa3edb46fea7f1d
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2024-01-21 19:13:43 +0500

    ɢᴅs-ᴍᴅ

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ const { name, author } = require(join(__dirname, './package.json'))
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
  
-say('Gata\nBot\nMD', {
+say('GDS\nMD', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
-say(`Project Author:\nGataNina-Li (@gata_dios)\n\nColaboradores:\nelrebelde21 (Mario)\nAzamiJs (Azami)\nKatashiFukushima (Katashi)`, {
+say(`Project Author:PRINCEGDS`, {
 font: 'console',
 align: 'center',
 gradient: ['red', 'magenta']})
@@ -49,7 +49,7 @@ break
 })
 p.on('exit', (_, code) => {
 isRunning = false;
-console.error('⚠️ ERROR ⚠️ >> ', code)
+console.error('⚠️ OHY ERROR ⚠️ >> ', code)
 p.process.kill()
 isRunning = false
 start.apply(this, arguments)

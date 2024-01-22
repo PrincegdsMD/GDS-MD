d5fce810b8d4b62250a31b6d11feb64da2f6a827
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2024-01-22 15:28:21 +0500

    ɢᴅs-ᴍᴅ

--- koyeb.js
+++ koyeb.js
@@ -5,7 +5,7 @@ RUN sudo dnf -y update &&\
     sudo dnf install -y git ffmpeg ImageMagick nodejs yarnpkg libwebp &&\
     sudo dnf clean all -y
 
-RUN git clone https://github.com/GataNina-Li/GataBot-MD
+RUN git clone https://github.com/PRINCE-GDS/GDS-MD
 
 WORKDIR /root/GataBot-MD
 

d0defabd274dc5ad1c5e80631eab089b29c9e3b0
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2024-01-24 08:25:53 +0500

    𝙂𝘿𝙎-𝙈𝘿

--- plugins/tools-banchat.js
+++ plugins/tools-banchat.js
@@ -3,7 +3,7 @@
 let handler = async (m, { conn, isOwner, isAdmin, isROwner }) => {
     if (!(isAdmin || isOwner)) return dfail('admin', m, conn)
     global.db.data.chats[m.chat].isBanned = true
-    m.reply('🟢Tʜᴇ ʙᴏᴛ ʜᴀs ʙᴇᴇɴ ᴅᴇᴀᴄᴛɪᴠᴀᴛᴇᴅ ғᴏʀ ᴛʜɪs ᴄʜᴀᴛ𒁂')
+    m.reply('*🟢Tʜᴇ ʙᴏᴛ ʜᴀs ʙᴇᴇɴ ᴅᴇᴀᴄᴛɪᴠᴀᴛᴇᴅ ғᴏʀ ᴛʜɪs ᴄʜᴀᴛ𒁂*')
 }
 handler.help = ['banchat']
 handler.tags = ['owner']

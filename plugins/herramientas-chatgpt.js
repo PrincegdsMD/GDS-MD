3b4c72f4257fad238a8ac364b7b1d50d4e3282e0
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2024-01-26 09:37:49 +0500

    𝙂𝘿𝙎-𝙈𝘿

--- plugins/herramientas-chatgpt.js
+++ plugins/herramientas-chatgpt.js
@@ -5,24 +5,33 @@ import { Configuration, OpenAIApi } from 'openai'
 const configuration = new Configuration({ organization: global.openai_org_id, apiKey: global.openai_key });
 const openaiii = new OpenAIApi(configuration);
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (usedPrefix == 'a' || usedPrefix == 'A') return    
-if (!text) throw `*${lenguajeGB['smsAvisoMG']()}𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙐𝙉𝘼 𝙋𝙀𝙏𝙄𝘾𝙄𝙊𝙉 𝙊 𝙐𝙉𝘼 𝙊𝙍𝘿𝙀𝙉 𝙋𝘼𝙍𝘼 𝙐𝙎𝘼𝙍 𝙇𝘼 𝙁𝙐𝙉𝘾𝙄𝙊𝙉 𝘿𝙀𝙇 𝘾𝙃𝘼𝙏𝙂𝙋𝙏\n\n❏ 𝙀𝙅𝙀𝙈𝙋𝙇𝙊 𝘿𝙀 𝙋𝙀𝙏𝙄𝘾𝙄𝙊𝙉𝙀𝙎 𝙔 𝙊𝙍𝘿𝙀𝙉𝙀𝙎\n❏ ${usedPrefix + command} Recomienda un top 10 de películas de acción\n❏ ${usedPrefix + command} Codigo en JS para un juego de cartas`     
-try {
+if (!text && !(m.quoted && m.quoted.text)) {
+  if (usedPrefix == 'a' || usedPrefix == 'A') return    
+if (!text) throw `*${lenguajeGB['smsAvisoMG']()}🧊𝙀𝙓𝘼𝙈𝙋𝙇𝙀: ${usedPrefix + command} 𝙒𝙝𝙖𝙩 𝙞𝙨 𝙄𝙨𝙡𝙖𝙢??`     
+ }
+  if (!text && m.quoted && m.quoted.text) {
+    text = m.quoted.text;
+  }
+  try {
+    m.react('⏳')
 conn.sendPresenceUpdate('composing', m.chat)  
-let syms = `Actuaras como un Bot de WhatsApp el cual fue creado por GataNina-Li, tu seras GataBot-MD`
+let syms = `You will act as a WhatsApp Bot which was created by PRINCE-GDS, you will be GDS-MD`
 let res = await gpt.ChatGpt(text, syms)
 await m.reply(res.text)
+    m.react('✅')
 } catch {
 try {   
 let ia2 = await fetch(`https://api.amosayomide05.cf/gpt/?question=${text}&string_id=${m.sender}`) //fetch(`https://api.ibeng.tech/api/info/openai?text=${text}&apikey=tamvan`)
 let resu2 = await ia2.json()
-m.reply(resu2.response.trim())    
+m.reply(resu2.response.trim()) 
+  m.react('✅')
 } catch {        
 try {    
 let tioress = await fetch(`https://api.lolhuman.xyz/api/openai-turbo?apikey=${lolkeysapi}&text=${text}`)
 let hasill = await tioress.json()
-m.reply(`${hasill.result}`.trim())   
+m.reply(`${hasill.result}`.trim())
+  m.react('✅')
 } catch {    
 }}}}
-handler.command = ['openai', 'chatgpt', 'ia', 'robot']
+handler.command = ['openai', 'gpt', 'ia', 'robot']
 export default handler

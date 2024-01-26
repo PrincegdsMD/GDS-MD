58cdb26d6ca0a82b5e5679c3852ea071fe714936
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2024-01-26 20:41:08 +0500

    𝙂𝘿𝙎-𝙈𝘿

--- plugins/propietario(a)-block-unblock.js
+++ plugins/propietario(a)-block-unblock.js
@@ -1,5 +1,5 @@
 let handler = async (m, { text, conn, usedPrefix, command }) => {
-let why = `*Ejemplo:*\n${usedPrefix + command} @${m.sender.split("@")[0]}`
+let why = `*🧊Example:*\n${usedPrefix + command} @${m.sender.split("@")[0]}`
 let who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : false
 if (!who) conn.reply(m.chat, why, m, { mentions: [m.sender] })
 let res = [];

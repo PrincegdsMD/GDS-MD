310f83f2b45469900bc163ba27718a9f53ef7cb3
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2024-01-29 18:07:29 +0500

    ɢᴅs-ᴍᴅ

--- plugins/sticker-wm.js
+++ plugins/sticker-wm.js
@@ -1,24 +1,29 @@
-import {addExif} from '../lib/sticker.js';
-const handler = async (m, {conn, text}) => {
-  if (!m.quoted) throw '🧊𝙍𝙀𝙋𝙇𝙔 𝙒𝙄𝙏𝙃 𝙎𝙏𝙄𝘾𝙆𝙀𝙍';
-  let stiker = false;
+
+import { addExif } from '../lib/sticker.js'
+let handler = async (m, { conn, text, args }) => {
+  if (!m.quoted) throw '🧊𝙍𝙀𝙋𝙇𝙔 𝙒𝙄𝙏𝙃 𝙎𝙏𝙄𝘾𝙆𝙀𝙍'
+  let packsticker = process.env.BOTNAME
+  let stickauthor = process.env.BOTNAME
+  let stiker = false
+       let stick = args.join(" ").split("|");
+       let f = stick[0] !== "" ? stick[0] : packsticker;
+       let g = typeof stick[1] !== "undefined" ? stick[1] : stickauthor;
   try {
-    let [packname, ...author] = text.split('|');
-    author = (author || []).join('|');
-    const mime = m.quoted.mimetype || '';
-    if (!/webp/.test(mime)) throw '🧊𝙍𝙀𝙋𝙇𝙔 𝙒𝙄𝙏𝙃 𝙎𝙏𝙄𝘾𝙆𝙀𝙍';
-    const img = await m.quoted.download();
-    if (!img) throw '🧊𝙍𝙀𝙋𝙇𝙔 𝙒𝙄𝙏𝙃 𝙎𝙏𝙄𝘾𝙆𝙀𝙍';
-    stiker = await addExif(img, packname || global.packname, author || global.author);
+    let mime = m.quoted.mimetype || ''
+    if (!/webp/.test(mime)) throw '🧊𝙍𝙀𝙋𝙇𝙔 𝙒𝙄𝙏𝙃 𝙎𝙏𝙄𝘾𝙆𝙀𝙍'
+    let img = await m.quoted.download()
+    if (!img) throw '🧊𝙍𝙀𝙋𝙇𝙔 𝙒𝙄𝙏𝙃 𝙎𝙏𝙄𝘾𝙆𝙀𝙍!'
+    stiker = await addExif(img, f, g)
   } catch (e) {
-    console.error(e);
-    if (Buffer.isBuffer(e)) stiker = e;
+    console.error(e)
+    if (Buffer.isBuffer(e)) stiker = e
   } finally {
-    if (stiker) conn.sendFile(m.chat, stiker, 'wm.webp', '', m, false, {asSticker: true});
-    else throw '[❗INFO❗] SORRY, SOMETHING WENT WRONG.. CHECK THAT YOU HAVE REPLIED TO A STICKER AND HAVE ADDED A PACKNAME AND USERNAME';
+    if (stiker) conn.sendFile(m.chat, stiker, 'wm.webp', '', m, null)
+     else throw 'conversion failed'
   }
-};
-handler.help = ['take <packname>|<author>'];
-handler.tags = ['sticker'];
-handler.command = /^take|robar|wm$/i;
-export default handler;
+}
+handler.help = ['take <name>|<author>']
+handler.tags = ['sticker']
+handler.command = ['take', 'wm'] 
+
+export default handler

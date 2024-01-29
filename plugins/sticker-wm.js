a6b9e998704e84319788a27a3878a3dafe6023db
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2024-01-29 17:52:27 +0500

    ɢᴅs-ᴍᴅ

--- plugins/sticker-wm.js
+++ plugins/sticker-wm.js
@@ -1,27 +1,24 @@
-
-import { addExif } from '../lib/sticker.js'
-let handler = async (m, { conn, text, args }) => {
-  if (!m.quoted) throw '🧊𝙍𝙀𝙋𝙇𝙔 𝙒𝙄𝙏𝙃 𝙎𝙏𝙄𝘾𝙆𝙀𝙍'
-  let stiker = false
-       let stick = args.join(" ").split("|");
-       let f = stick[0] !== "" ? stick[0] : packname;
-       let g = typeof stick[1] !== "undefined" ? stick[1] : author;
+import {addExif} from '../lib/sticker.js';
+const handler = async (m, {conn, text}) => {
+  if (!m.quoted) throw '🧊𝙍𝙀𝙋𝙇𝙔 𝙒𝙄𝙏𝙃 𝙎𝙏𝙄𝘾𝙆𝙀𝙍';
+  let stiker = false;
   try {
-    let mime = m.quoted.mimetype || ''
-    if (!/webp/.test(mime)) throw '🧊𝙍𝙀𝙋𝙇𝙔 𝙒𝙄𝙏𝙃 𝙎𝙏𝙄𝘾𝙆𝙀𝙍'
-    let img = await m.quoted.download()
-    if (!img) throw 'Responde to sticker!'
-    stiker = await addExif(img, f, g)
+    let [packname, ...author] = text.split('|');
+    author = (author || []).join('|');
+    const mime = m.quoted.mimetype || '';
+    if (!/webp/.test(mime)) throw '🧊𝙍𝙀𝙋𝙇𝙔 𝙒𝙄𝙏𝙃 𝙎𝙏𝙄𝘾𝙆𝙀𝙍';
+    const img = await m.quoted.download();
+    if (!img) throw '🧊𝙍𝙀𝙋𝙇𝙔 𝙒𝙄𝙏𝙃 𝙎𝙏𝙄𝘾𝙆𝙀𝙍';
+    stiker = await addExif(img, packname || global.packname, author || global.author);
   } catch (e) {
-    console.error(e)
-    if (Buffer.isBuffer(e)) stiker = e
+    console.error(e);
+    if (Buffer.isBuffer(e)) stiker = e;
   } finally {
-    if (stiker) conn.sendFile(m.chat, stiker, 'wm.webp', '', m, null)
-     else throw 'conversion failed'
+    if (stiker) conn.sendFile(m.chat, stiker, 'wm.webp', '', m, false, {asSticker: true});
+    else throw '[❗INFO❗] SORRY, SOMETHING WENT WRONG.. CHECK THAT YOU HAVE REPLIED TO A STICKER AND HAVE ADDED A PACKNAME AND USERNAME';
   }
-}
-handler.help = ['take <name>|<author>']
-handler.tags = ['sticker']
-handler.command = ['take', 'wm'] 
-
-export default handler
+};
+handler.help = ['take <packname>|<author>'];
+handler.tags = ['sticker'];
+handler.command = /^take|robar|wm$/i;
+export default handler;

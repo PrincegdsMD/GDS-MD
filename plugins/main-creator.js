adbfdc6175c7e7c0f1e8217c76236452cac5f8b1
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2024-01-30 14:40:50 +0500

    𝙂𝘿𝙎-𝙈𝘿

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,13 +1,24 @@
-function handler(m) {
-  let powner = process.env.OWNERNAME
-  
-  const data = global.powner(([id, isCreator]) => id && isCreator)
-  this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
+var handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
+let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+let pp = gataImg.getRandom()	
+const cat = `*🩵𝙂𝘿𝙎-𝘽𝙊𝙏-𝙈𝘿🩵*
 
+𝙉𝘼𝙈𝙀: 𝙋𝙍𝙄𝙉𝘾𝙀💗
+*_—◉ ɢᴅs-ᴍᴅ ʙᴏᴛ ᴏᴡɴᴇʀ 𝟷 wa.me/923092668108_*
+
+
+𝙉𝘼𝙈𝙀: 𝙋𝙍𝙄𝙉𝘾𝙀💗
+*_—◉ ɢᴅs-ᴍᴅ ʙᴏᴛ ᴏᴡɴᴇʀ 2 wa.me/92Your second number here_*
+*---------------------*
+
+
+*_ᴛʜᴇ ᴍᴏᴅᴇʀᴀᴛᴏʀ_*
+*${developer}*`
+
+await conn.sendFile(m.chat, pp, 'menuvid', cat, fkontak)
 }
-
-handler.help = ['owner']
-handler.tags = ['main']
-handler.command = ['owner', 'creator', 'creador', 'dueño', 'Gowner'] 
+handler.help = ['owner', 'creator']
+handler.tags = ['info']
+handler.command = /^(owner|creator|propietario|dueño|dueña|propietaria|dueño|creadora|creador)$/i
 
 export default handler

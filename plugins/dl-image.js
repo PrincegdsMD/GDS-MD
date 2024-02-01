be448c4b27684d86db09eff4b10d95071140d800
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2024-02-01 19:30:39 +0500

    𝙂𝘿𝙎-𝙈𝘿

---
+++ plugins/dl-image.js
@@ -0,0 +1,14 @@
+import { googleImage } from '@bochilteam/scraper'
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+if (!text) throw `${lenguajeGB['smsAvisoMG']()}𝙐𝙎𝙀 𝙏𝙃𝙀 𝘾𝙊𝙈𝙈𝘼𝙉𝘿 𝙇𝙄𝙆𝙀 𝙏𝙃𝙄𝙎\n*${usedPrefix + command} Nature*`
+const prohibited = ['fuck', 'caca', 'polla', 'porno', 'porn', 'gore', 'cum', 'semen', 'puta', 'puto', 'culo', 'putita', 'putito','pussy', 'hentai', 'pene', 'coño', 'asesinato', 'zoofilia', 'mia khalifa', 'desnudo', 'desnuda', 'cuca', 'chocha', 'muertos', 'pornhub', 'xnxx', 'xvideos', 'teta', 'vagina', 'marsha may', 'misha cross', 'sexmex', 'furry', 'furro', 'furra', 'xxx', 'rule34', 'panocha', 'pedofilia', 'necrofilia', 'pinga', 'horny', 'ass', 'nude', 'popo', 'nsfw', 'femdom', 'futanari', 'erofeet', 'sexo', 'sex', 'yuri', 'ero', 'ecchi', 'blowjob', 'anal', 'ahegao', 'pija', 'verga', 'trasero', 'violation', 'violacion', 'bdsm', 'cachonda', '+18', 'cp', 'mia marin', 'lana rhoades', 'cepesito', 'hot', 'buceta', 'xxx']
+if (prohibited.some(word => m.text.toLowerCase().includes(word))) return m.reply('*⚠️OHY Lanati, GDS-MD ak Muslim bot hai gatya images support nhi krta*')      
+const res = await googleImage(text)
+let image = res.getRandom()
+let link = image
+conn.sendFile(m.chat, link, 'error.jpg', `*💞𝙍𝙚𝙨𝙪𝙡𝙩: ${text}*`, m)
+}
+handler.help = ['gimage <query>', 'imagen <query>']
+handler.tags = ['internet', 'Downloader']
+handler.command = /^(gimage|image|imagen|img)$/i
+export default handler

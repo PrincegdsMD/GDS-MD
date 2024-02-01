312492e951bc6f8fadb947434e789b02850a781a
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2024-02-01 18:17:03 +0500

    𝙂𝘿𝙎-𝙈𝘿

--- plugins/dl-tiktokimg.js
+++ plugins/dl-tiktokimg.js
@@ -1,9 +1,9 @@
 import fetch from 'node-fetch'
 
 let handler = async(m, { conn, text, usedPrefix, command }) => {
-if (!text) throw `*⚠️ INGRESE UN ENLACE DE TIKTOK QUE CONTENGA IMÁGENES.* 😿`
+if (!text) throw `*⚠️ ENTER A TIKTOK LINK CONTAINING IMAGES.*`
 if (!(text.includes('http://') || text.includes('https://'))) return m.reply(`url invalid, please input a valid url. Try with add http:// or https://`)
-if (!text.includes('tiktok.com')) return m.reply(`*⚠️ URL INVALIDA.*`)
+if (!text.includes('tiktok.com')) return m.reply(`*⚠️ URL INVALIDATES.*`)
 try {
 let res = await fetch(`https://api.lolhuman.xyz/api/tiktokslide?apikey=${global.lolkeysapi}&url=${text}`)
 let anu = await res.json()
@@ -12,18 +12,18 @@ anu = anu.result
 if (anu.length == 0) throw Error('Error : no data')
 let c = 0
 for (let x of anu) {
-if (c == 0) await conn.sendMessage(m.chat, { image: { url: x }, caption: `✅ *Se ha enviado 1 de ${anu.length} imágenes.* ✅\n_El resto podrá ser visible en el chat privado del bot_ 😸` }, { quoted : m })
+if (c == 0) await conn.sendMessage(m.chat, { image: { url: x }, caption: `✅ *1 of ${anu.length} imagery.* ✅\n_The rest will be visible in the bot's private chat_ 😸` }, { quoted : m })
 else await conn.sendMessage(m.sender, { image: { url: x } }, { quoted : m })
 c += 1
 }
 } catch (e) {
 console.log(e)
-throw `*⚠️ ERROR, INTENTE DE NUEVO.*`
+throw `*⚠️ ERROR, TRY AGAIN.*`
 }}
 
 handler.menu = ['tiktokslide <url>']
 handler.tags = ['search']
-handler.command = /^((tt|tiktok)imagen)$/i
+handler.command = /^((tt|tiktok)img)$/i
 
 handler.premium = true
 handler.limit = true

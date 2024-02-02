5adf7641109efe46b3147be4678e58759ac1edad
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2024-02-03 02:05:56 +0500

    𝙂𝘿𝙎-𝙈𝘿

--- plugins/convertor-tourl.js
+++ plugins/convertor-tourl.js
@@ -11,14 +11,14 @@ let pp = await conn.profilePictureUrl(who).catch(_ => gataImg.getRandom())
 let name = await conn.getName(who)
 let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || ''
-if (!mime) throw `${mg} 𝙍𝙀𝙎𝙋𝙊𝙉𝘿𝘼 𝘼 𝙐𝙉𝘼 𝙄𝙈𝘼𝙂𝙀𝙉 𝙊 𝙑𝙄𝘿𝙀𝙊\n𝙍𝙀𝙎𝙋𝙊𝙉𝘿 𝙏𝙊 𝘼𝙉 𝙄𝙈𝘼𝙂𝙀 𝙊𝙍 𝙑𝙄𝘿𝙀𝙊`
+if (!mime) throw `${mg} ʀᴇᴘʟʏ ᴡɪᴛʜ ɪᴍᴀɢᴇ ᴏʀ ᴠɪᴅᴇᴏ ᴛᴏ ɢᴇᴛ ᴛʜᴇ ᴜʀʟ`
 let media = await q.download()
 let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
 let link = await (isTele ? uploadImage : uploadFile)(media)
-let caption = `🛑 𝙀𝙉𝙇𝘼𝘾𝙀:\n${link}\n🥏 𝙏𝘼𝙈𝘼𝙉𝙊: ${media.length}\n🚀 𝙀𝙓𝙋𝙄𝙍𝘼𝘾𝙄𝙊𝙉: ${isTele ? '𝙉𝙊 𝙀𝙓𝙋𝙄𝙍𝘼' : '𝘿𝙀𝙎𝘾𝙊𝙉𝙊𝘾𝙄𝘿𝙊'}\n🔰 𝘼𝘾𝙊𝙍𝙏𝘼𝘿𝙊: ${await shortUrl(link)}`
+let caption = `🛑 𝙇𝙄𝙉𝙆:\n${link}\n🥏 𝙎𝙄𝙕𝙀: ${media.length}\n🚀 𝑬𝑿𝑷𝑰𝑹𝑨𝑻𝑰𝑶𝑵: ${isTele ? '𝙉𝙊 𝙀𝙓𝙋𝙄𝙍𝘼𝙏𝙄𝙊𝙉' : '𝙐𝙉𝙆𝙉𝙊𝙒𝙉'}\n🔰 𝙎𝙃𝙊𝙍𝙏𝙀𝙉𝙀𝘿: ${await shortUrl(link)}`
 conn.reply(m.chat, caption, m, { contextInfo: {externalAdReply :{mediaUrl: md, mediaType: 2, title: wm, body: botdate, thumbnail: await(await fetch(link)).buffer(), sourceUrl: link }}})}
 handler.help = ['tourl']
-handler.tags = ['herramientas']
+handler.tags = ['tools']
 handler.command = /^(tourl|upload)$/i
 export default handler
 

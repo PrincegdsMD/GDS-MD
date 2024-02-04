3b0d07192bab5244becfafb70a213f5c3c677209
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2024-02-04 08:14:34 +0500

    𝙂𝘿𝙎-𝙈𝘿

--- plugins/_simi.js
+++ plugins/_simi.js
@@ -15,9 +15,9 @@ if (data.success == 'No s\u00e9 lo qu\u00e9 est\u00e1s diciendo. Por favor ense\
 await m.reply(data.success)
 } catch {
 /* SI DA ERROR USARA ESTA OTRA OPCION DE API DE IA QUE RECUERDA EL NOMBRE DE LA PERSONA */
-if (textodem.includes('Hola')) textodem = textodem.replace('Hola', 'Hello')
-if (textodem.includes('hola')) textodem = textodem.replace('hola', 'hello')
-if (textodem.includes('HOLA')) textodem = textodem.replace('HOLA', 'HELLO')    
+if (textodem.includes('Hello')) textodem = textodem.replace('Hello', 'Hello')
+if (textodem.includes('Hello')) textodem = textodem.replace('Hello', 'hello')
+if (textodem.includes('Hello')) textodem = textodem.replace('Hello', 'HELLO')    
 let reis = await fetch("https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q=" + textodem)
 let resu = await reis.json()  
 let nama = m.pushName || '1'

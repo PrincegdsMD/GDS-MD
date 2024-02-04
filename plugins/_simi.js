0dc22bb9eb66534f4af177532d2b460cd54da419
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2024-02-04 08:18:38 +0500

    𝙂𝘿𝙎-𝙈𝘿

--- plugins/_simi.js
+++ plugins/_simi.js
@@ -9,7 +9,7 @@ if (!m.text) return
 let textodem = m.text  
 try {
 await conn.sendPresenceUpdate('composing', m.chat)
-let ressimi = await fetch(`https://api.simsimi.net/v2/?text=${encodeURIComponent(m.text)}&lc=` + lenguajeGB.lenguaje())
+let ressimi = await fetch(`https://api.simsimi.net/v2/?text=${encodeURIComponent(m.text)}&lc=` + "en"
 let data = await ressimi.json();
 if (data.success == 'No s\u00e9 lo qu\u00e9 est\u00e1s diciendo. Por favor ense\u00f1ame.') return m.reply(`${lol}`) /* EL TEXTO "lol" NO ESTA DEFINIDO PARA DAR ERROR Y USAR LA OTRA API */
 await m.reply(data.success)

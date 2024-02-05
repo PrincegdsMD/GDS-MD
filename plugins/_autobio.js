ac4d1c13b496bb35ca210b1d4ace1acdcdec1679
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2024-02-05 09:17:26 +0500

    𝙂𝘿𝙎-𝙈𝘿

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -1,19 +1,22 @@
 let handler = m => m
 handler.all = async function (m) {
-let setting = global.db.data.settings[this.user.jid]
-
-let bot = global.db.data.settings[this.user.jid] || {};
+	let setting = global.db.data.settings[this.user.jid]
+	
+  let bot = global.db.data.settings[this.user.jid] || {};
   if (bot.autoBio) {
-let _uptime = process.uptime() * 1000
-let _muptime
-if (process.send) { process.send('uptime')
-_muptime = await new Promise(resolve => { process.once('message', resolve) 
-setTimeout(resolve, 2000) }) * 1000}
-let uptime = clockString(_uptime)
-let bio = `${global.packname} ║ ✅ ${uptime} ⌛ ║ 💗Love you 💻 ɢᴅs-ᴍᴅ💗 `
-await this.updateProfileStatus(bio).catch(_ => _)
-setting.status = new Date() * 1
-} 
+    let _muptime
+    if (process.send) {
+      process.send('uptime')
+      _muptime = await new Promise(resolve => {
+        process.once('message', resolve)
+        setTimeout(resolve, 1000)
+      }) * 1000
+    }
+    let muptime = clockString(_muptime)
+		let bio = `${global.packname}\n⌚ Time Active ${muptime}\n ┃┃ 💗Love you 💻 ɢᴅs-ᴍᴅ💗 `
+		await this.updateProfileStatus(bio).catch(_ => _)
+		setting.status = new Date() * 1
+  }
 }
 export default handler
 
@@ -22,5 +25,5 @@ function clockString(ms) {
   let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
   let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return [d, ' » ', h, ' ・ ', m, ' ・ ', s].map(v => v.toString().padStart(2, 0)).join('') 
-} 
+  return [d, ' Day(s) ️', h, ' Hour(s) ', m, ' Minute(s)'].map(v => v.toString().padStart(2, 0)).join('')
+}
